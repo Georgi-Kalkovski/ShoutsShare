9d48a4468fbd1be0309fdafe7580c7934102352e
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2020-11-07 01:47:18 +0200

    Adding bootstrap template

---
+++ Data/ShoutsShare.Data.Models/ProfilePicture.cs
@@ -0,0 +1,14 @@
+﻿using ShoutsShare.Data.Common.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoutsShare.Data.Models
+{
+    public class ProfilePicture : BaseDeletableModel<int>
+    {
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
--- Data/ShoutsShare.Data.Models/UserContent.cs
+++ Data/ShoutsShare.Data.Models/UserContent.cs
@@ -1,9 +1,6 @@
 ﻿using ShoutsShare.Data.Common.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace ShoutsShare.Data.Models
 {
